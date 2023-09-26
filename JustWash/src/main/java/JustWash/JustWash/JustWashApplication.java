4f2f444ecb395c8a0abe338a9830dea048a57e06
6531503060 <6531503060@lamduamn.mfu.ac.th>
2023-09-26 23:42:47 +0700

    KY

---
+++ JustWash/src/main/java/JustWash/JustWash/JustWashApplication.java
@@ -0,0 +1,13 @@
+package JustWash.JustWash;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JustWashApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JustWashApplication.class, args);
+	}
+
+}
---
+++ JustWash/src/test/java/JustWash/JustWash/JustWashApplicationTests.java
@@ -0,0 +1,13 @@
+package JustWash.JustWash;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JustWashApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
